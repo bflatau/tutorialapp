c24eb6b212302cc3f4ebb1b77c028b429c0d21d2
ben <Herbie Hancock@.(none)>
2011-04-10 04:55:18 +0300

    tutorial app base configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorialapp::Application.config.secret_token = '646b66797fb487115bf20830526cd548817696bd3027a325c1ab11dcb4b2b5d8d4e3661ce1fb24a5ef85b399049f6824858e135d24c81569b0e4e82ff478c75f'
